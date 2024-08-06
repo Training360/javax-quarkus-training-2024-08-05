b86d159ee157736ff09ebd61dfe715c8dde4c9c5
vicziani <viczian.istvan@gmail.com>
2024-08-06 09:17:38 +0200

    Átállás entitásokra

--- employees/src/main/java/employees/EmployeesService.java
+++ employees/src/main/java/employees/EmployeesService.java
@@ -6,25 +6,35 @@ import training.dto.EmployeeDto;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @ApplicationScoped
 public class EmployeesService {
 
-    private List<EmployeeDto> employees = Collections.synchronizedList(
+    private AtomicLong sequence = new AtomicLong();
+
+    private List<Employee> employees = Collections.synchronizedList(
             new ArrayList<>(List.of(
-                    new EmployeeDto().id(1L).name("John Doe"),
-                    new EmployeeDto().id(2L).name("Jane Doe")
+                    new Employee(sequence.incrementAndGet(), "John Doe"),
+                    new Employee(sequence.incrementAndGet(), "Jane Doe")
             ))
     );
 
+    private static EmployeeDto toDto(Employee entity) {
+        return new EmployeeDto().id(entity.getId()).name(entity.getName());
+    }
+
     public List<EmployeeDto> listEmployees() {
-        return employees;
+        return employees.stream()
+                .map(EmployeesService::toDto)
+                .toList();
     }
 
     public EmployeeDto findEmployeeById(Long id) {
         return employees.stream()
                 .filter(employee -> employee.getId().equals(id))
                 .findAny()
+                .map(EmployeesService::toDto)
                 .orElseThrow(() -> new IllegalArgumentException("Employee not found  + id"));
     }
 }
