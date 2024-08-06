41e95e7a16f6920c9661a860b412ea52a7591118
vicziani <viczian.istvan@gmail.com>
2024-08-06 16:15:14 +0200

    Listázás tesztelése, AssertJ-vel

--- employees/src/test/java/employees/EmployeeResourceTest.java
+++ employees/src/test/java/employees/EmployeeResourceTest.java
@@ -2,6 +2,7 @@ package employees;
 
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
@@ -9,9 +10,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import training.dto.EmployeeDto;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -44,5 +47,31 @@ class EmployeeResourceTest {
         assertEquals("John Doe", response.getName());
     }
 
+    @Test
+    void listEmployees() {
+        when(employeesService.listEmployees(any())).thenReturn(
+                List.of(
+                        new EmployeeDto().id(1L).name("John Doe"),
+                        new EmployeeDto().id(2L).name("Jane Doe")
+                )
+        );
 
+        List<EmployeeDto> employees = given()
+                .when()
+                .get("/api/employees")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().body().as(new TypeRef<>() {});
+
+//        assertLinesMatch(List.of("John Doe", "Jane Doe"),
+//            employees.stream().map(EmployeeDto::getName).toList());
+
+//        assertEquals(List.of("John Doe", "Jane Doe"),
+//                employees.stream().map(EmployeeDto::getName).toList());
+
+        assertThat(employees)
+                .extracting(EmployeeDto::getName)
+                .containsExactly("John Doe", "Jane Doe");
+
+    }
 }
